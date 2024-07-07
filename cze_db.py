af890389417201b27dddd437111ea35afa3917cd
rope07 <ppaskovic@gmail.com>
2024-07-08 01:28:58 +0200

    sortiranje u hrv searchu po češkoj riječi

--- cze_db.py
+++ cze_db.py
@@ -12,36 +12,13 @@ def czech_collation(str1, str2):
     str1 = str1.replace('ch', 'hžžžž')
     str2 = str2.replace('ch', 'hžžžž')
     
-    # Compare the strings character by character based on Czech alphabet order
     for c1, c2 in zip(str1, str2):
         if c1 != c2:
             pos1 = czech_order.find(c1)
             pos2 = czech_order.find(c2)
             return pos1 - pos2
     
-    # If one string is a prefix of the other, the shorter string is considered smaller
     return len(str1) - len(str2)
-
-def croatian_collation(str1, str2):
-    croatian_order = " aabcčćdđdefghijklmnoprstuvzž"
-
-    def transform_string(s):
-        s = s.replace('dž', 'dz~')  # Use a character sequence that comes after 'dz'
-        s = s.replace('lj', 'l~')   # Use a character sequence that comes after 'l'
-        s = s.replace('nj', 'n~')   # Use a character sequence that comes after 'n'
-        return s
-    
-    transformed_str1 = transform_string(str1)
-    transformed_str2 = transform_string(str2)
-
-    
-    for c1, c2 in zip(transformed_str1, transformed_str2):
-        if c1 != c2:
-            pos1 = croatian_order.find(c1)
-            pos2 = croatian_order.find(c2)
-            return pos1 - pos2
-    
-    return len(transformed_str1) - len(transformed_str2)
 
 def create_table():
     conn = sqlite3.connect(db_path)
@@ -85,10 +62,10 @@ def search_words(lookup_record):
 
 def search_croatian(lookup_record):
     conn = sqlite3.connect(db_path)
-    conn.create_collation("CROATIAN", croatian_collation)
+    conn.create_collation("CZECH", czech_collation)
     cursor = conn.cursor()
 
-    cursor.execute("SELECT rowid, * FROM dictionary WHERE cro_word like ? ORDER BY cro_word COLLATE CROATIAN", (lookup_record,))
+    cursor.execute("SELECT rowid, * FROM dictionary WHERE cro_word like ? ORDER BY cze COLLATE CZECH", (lookup_record,))
     
     words = cursor.fetchall()
     conn.commit()
